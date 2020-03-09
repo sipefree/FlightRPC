e79cec7d46892ee119a6e825e3b7618bc78c5a94
Simon Free <sfree@apple.com>
2020-03-08 18:49:20 -0700

    0.0.1 - fixed

--- Package.swift
+++ Package.swift
@@ -10,28 +10,28 @@ let package = Package(
         .library(
             name: "FlightRPC",
             targets: ["FlightRPC"]),
-        .executable(
-            name: "flightrpc-gen",
-            targets: ["flightrpc-gen"])
+        //.executable(
+            //name: "flightrpc-gen",
+            //targets: ["flightrpc-gen"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.0.1"),
-        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.3.0"),
-        .package(url: "https://github.com/mxcl/Path.swift.git", from: "1.0.1"),
-        .package(url: "https://github.com/vapor/console-kit.git", .branch("4.0.0-rc.1")),
+        //.package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.0.1"),
+        //.package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.3.0"),
+        //.package(url: "https://github.com/mxcl/Path.swift.git", from: "1.0.1"),
+        //.package(url: "https://github.com/vapor/console-kit.git", .branch("4.0.0-rc.1")),
     ],
     targets: [
         .target(
             name: "FlightRPC",
             dependencies: []),
-        .target(
-            name: "flightrpc-gen",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Path", package: "Path.swift"),
-                .product(name: "ShellOut", package: "ShellOut"),
-                .product(name: "ConsoleKit", package: "console-kit"),
-            ]),
+//        .target(
+//            name: "flightrpc-gen",
+//            dependencies: [
+//                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+//                .product(name: "Path", package: "Path.swift"),
+//                .product(name: "ShellOut", package: "ShellOut"),
+//                .product(name: "ConsoleKit", package: "console-kit"),
+//            ]),
         .testTarget(
             name: "FlightRPCTests",
             dependencies: ["FlightRPC"]),
